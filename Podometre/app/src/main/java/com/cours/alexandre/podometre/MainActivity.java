6de864a5ec14803db384bee08ed7da096d8625bd
AlexandreUPMF <gannouchedu86@gmail.com>
2016-11-14 10:38:22 +0100

    Ajout du tableau pour calcul seuil

--- Podometre/app/src/main/java/com/cours/alexandre/podometre/MainActivity.java
+++ Podometre/app/src/main/java/com/cours/alexandre/podometre/MainActivity.java
@@ -11,11 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity implements SensorEventListener {
 
 
     private SensorManager mSensorManager;
     private Sensor flash ;
+    private ArrayList<Float> tableauPics;
+    private float seuil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,8 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         mSensorManager = ( SensorManager ) getSystemService (Context.SENSOR_SERVICE);
         flash = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
+        tableauPics = new ArrayList<>();
+        seuil = 0;
     }
 
     @Override
@@ -48,11 +54,30 @@ public class MainActivity extends Activity implements SensorEventListener {
         TextView normAcc = (TextView) findViewById(R.id.normAccExt);
         normAcc.setText( "" + normAccExt);
 
+        if(seuil < normAccExt) {
+            tableauPics.add(normAccExt);
+            seuil = normAccExt;
+        }
+        else
+        {
+            TextView tableauPic = (TextView) findViewById(R.id.tableauPic);
+            for (int i = tableauPics.size() - 1 ; i == tableauPics.size() - 11; i--) {
+                tableauPic.append("" +tableauPics.get(i));
+            }
+        }
+
+
+        TextView textSeuil = (TextView) findViewById(R.id.seuil);
+        textSeuil.setText("" + this.seuil);
+
+
+
+
     }
 
     protected void onResume () {
         super.onResume();
-        mSensorManager.registerListener(this, flash, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, flash, 500);
     }
 
     protected void onPause () {
