54f0e7a81fc46681331e9bb6ff8af67d15732070
AlexandreUPMF <gannouchedu86@gmail.com>
2016-11-28 11:01:03 +0100

    Vidage du main

--- Podometre/app/src/main/java/com/cours/alexandre/podometre/MainActivity.java
+++ Podometre/app/src/main/java/com/cours/alexandre/podometre/MainActivity.java
@@ -13,31 +13,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import java.util.Date;
 
-public class MainActivity extends Activity implements SensorEventListener, View.OnClickListener {
+public class MainActivity extends Activity implements  View.OnClickListener {
 
-
-    private SensorManager mSensorManager;
-    private Sensor flash ;
-    private float seuil;
-    private int nbPas;
-    private boolean passageSeuil;
-    private Date delay;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mSensorManager = ( SensorManager ) getSystemService (Context.SENSOR_SERVICE);
-        flash = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        seuil = 2.56F;
-        nbPas = 0;
-        passageSeuil = false;
-        delay = new Date();
-
-        Button resetbtn = (Button) findViewById(R.id.resetButton);
-        resetbtn.setOnClickListener(this);
 
         Button VueBoussole = (Button) findViewById(R.id.boussole);
         VueBoussole.setOnClickListener(this);
@@ -47,74 +30,18 @@ public class MainActivity extends Activity implements SensorEventListener, View.
 
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        // On calcul la norme de l'accélération
-        float normAccExt = calculNorme(event);
-
-        // On récupére le temps courrant pour le calcul du delay
-        Date delayUp = new Date();
-
-        // On calcul un delay pour ne compter les pas que toutes les 500ms
-        long diff = delayUp.getTime() - delay.getTime();
-
-        if(passageSeuil == false && normAccExt > seuil) {
-            passageSeuil = true;
-        }
-        if(passageSeuil == true && normAccExt < seuil && diff > 500) {
-            nbPas++;
-            passageSeuil = false;
-
-            // On reinit le delay de prise
-            delay = new Date();
-
-            TextView Pas = (TextView) findViewById(R.id.nbPas);
-            Pas.setText("" + this.nbPas);
-        }
-    }
-
     protected void onResume () {
         super.onResume();
-        mSensorManager.registerListener(this, flash, SensorManager.SENSOR_DELAY_GAME);
     }
 
     protected void onPause () {
         super.onPause();
-        mSensorManager.unregisterListener(this);
     }
 
-    public float calculNorme(SensorEvent event) {
-
-        float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
-
-
-        float normAccExt = (float)(Math.sqrt((x*x) + (y*y) + (z*z)) - 9.8);
-
-        return normAccExt;
-    }
-
-    public void ResetNbPas() {
-        nbPas = 0;
-        TextView Pas = (TextView) findViewById(R.id.nbPas);
-        Pas.setText("" + this.nbPas);
-        delay = new Date();
-    }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
-
-        if(id == R.id.resetButton) {
-            ResetNbPas();
-        }
 
         if(id == R.id.boussole) {
             // Explicit Intent by specifying its class name
