c10e29ab63e0ccc1542eca4399cf2f9feebb363b
AlexandreUPMF <gannouchedu86@gmail.com>
2016-11-28 11:45:38 +0100

    Ajout de la MapBox

--- Podometre/app/src/main/java/com/cours/alexandre/podometre/map.java
+++ Podometre/app/src/main/java/com/cours/alexandre/podometre/map.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class map extends Activity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,48 @@ public class map extends Activity {
         MapboxAccountManager.start(this, getString(R.string.access_token));
         setContentView(R.layout.activity_map);
 
-        
+        // Create a mapView
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                // Customize map with markers, polylines, etc.
+            }
+        });
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+
+
 }
